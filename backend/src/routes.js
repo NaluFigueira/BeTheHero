72635317e7d392ad986bc7527f1ec0d5ea18e8e7
NaluFigueira <anachfigueira@gmail.com>
2020-04-06 23:37:13 -0300

    Session routes completed

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
+const SessionController = require('./controllers/SessionController');
 const IncidentController = require('./controllers/IncidentController');
 const OngController = require('./controllers/OngController');
+const ProfileController = require('./controllers/ProfileController');
 
 const routes = express.Router();
+
+routes.post('/sessions', SessionController.store);
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.store);
@@ -11,5 +15,7 @@ routes.post('/ongs', OngController.store);
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.store);
 routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
