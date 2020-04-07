3c85568ad57d4a5ff67dd95c36c2789dacc0eda2
NaluFigueira <anachfigueira@gmail.com>
2020-04-06 23:11:23 -0300

    Adding controller for ONGs and applying corrections on app index

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
+const OngController = require('./controllers/OngController');
+
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-  return response.send('Hello World');
-})
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.store);
+
+module.exports = routes;
