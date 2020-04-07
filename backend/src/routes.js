a5f0aef975490fbe7bc557749d161085d0bfe4fa
NaluFigueira <anachfigueira@gmail.com>
2020-04-06 23:27:00 -0300

    Incident Controller completed

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 
+const IncidentController = require('./controllers/IncidentController');
 const OngController = require('./controllers/OngController');
 
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.store);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.store);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
