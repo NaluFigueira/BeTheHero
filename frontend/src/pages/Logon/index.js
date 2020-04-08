322a6a586e369d586c4a45e992784a6a98e2ce46
NaluFigueira <anachfigueira@gmail.com>
2020-04-07 21:58:35 -0300

    Adding routing

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
-import './styles';
+import './styles.css';
 
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
@@ -18,10 +19,10 @@ export default function Logon() {
           <input placeholder="Sua ID"/>
           <button className="button" type="submit">Entrar</button>
 
-          <a href="/register">
+          <Link to="/register">
             <FiLogIn size={16} color="#E02041" />
             Não tenho cadastro
-          </a>
+          </Link>
         </form>
       </section>
 
